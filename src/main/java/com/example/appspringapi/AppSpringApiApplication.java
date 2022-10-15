ebe50c250bc68d50b7d82309a46141a7e3edc2fd
Nicolas vegas <nicolas_12_acuario@hotmail.com>
2022-10-14 19:08:06 -0500

    Nicolas:First Commit

---
+++ src/main/java/com/example/appspringapi/AppSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.appspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appspringapi/AppSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
